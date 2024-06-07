c96fd9dc0ab346042055952477d3d21a21992a7a
Allen Faure <afaure6@gmail.com>
2024-06-07 18:55:56 -0500

    add all the other crap in. next step is summaries on non-leaf nodes, and then make it pretty

--- core.lua
+++ core.lua
@@ -2,16 +2,37 @@
 
 ---@type string
 local name = ...
+
 ---@class ns
+---@field data CheckListData
 local ns = select(2, ...)
 
----@class Panel : Frame
-local Panel = CreateFrame("Frame", "RemixWeaponsPanel", UIParent, "DefaultPanelTemplate")
-Panel:EnableMouse(true)
-Panel:SetMovable(true)
-Panel:SetSize(450, 300)
-Panel:SetPoint("CENTER")
-Panel:Hide()
+---@class CheckListData
+---@field weapons table<equip, table<itemID, WeaponCheckListItem>>
+---@field appearances table<vendor, table<itemID, AppearanceCheckListItem>>
+---@field toys table<itemID, CheckListItem>
+---@field mounts table<itemID, CheckListItem>
+
+---@alias vendor
+---| '"world"'
+---| '"dungeon"'
+---| '"lfr"'
+---| '"normal"'
+---| '"heroic"'
+---| '"bones"'
+
+---@class CheckListItem
+---@field link string
+---@field has boolean
+---@field cost number
+
+---@class WeaponCheckListItem : CheckListItem
+---@field location string
+---@field cost nil
+
+---@class AppearanceCheckListItem : CheckListItem
+---@field remaining number
+
 
 ---@type RemixWeaponsFrame
 local RemixWeaponsFrame
@@ -23,6 +44,122 @@ SlashCmdList["REMIXWEAPONS"] = function()
    else
       RemixWeaponsFrame:SetShown(not RemixWeaponsFrame:IsShown())
    end
+   if not ns.loaded then
+      ns:LoadItemData(function() RemixWeaponsFrame:Populate() end)
+   end
 end
 
+---@return boolean?, number?
+local function hasEnsemble(itemID)
+   local setID = C_Item.GetItemLearnTransmogSet(itemID)
+   if not setID then return end
+   local setItems = C_Transmog.GetAllSetAppearancesByID(setID)
+   if not setItems then return end
+   local count = 0
+   local slotsUnlearned = {}
+   for i, itemData in ipairs(setItems) do
+      if not C_TransmogCollection.PlayerHasTransmogItemModifiedAppearance(itemData.itemModifiedAppearanceID) then
+         if not slotsUnlearned[itemData.invSlot] then
+            count = count + 1
+         end
+         slotsUnlearned[itemData.invSlot] = true
+      else
+         if slotsUnlearned[itemData.invSlot] then
+            count = count - 1
+         end
+         slotsUnlearned[itemData.invSlot] = nil
+      end
+   end
+   return next(slotsUnlearned) == nil, count
+end
 
+function ns:CreateItems()
+   self.items = {}
+   for _, ids in pairs(ns.weapons) do
+      for id in pairs(ids) do
+         table.insert(self.items, Item:CreateFromItemID(id))
+      end
+   end
+   for _, ids in pairs(ns.appearances) do
+      for _, data in pairs(ids) do
+         table.insert(self.items, Item:CreateFromItemID(data[1]))
+      end
+   end
+   for _, mountInfo in pairs(ns.mounts) do
+      table.insert(self.items, Item:CreateFromItemID(mountInfo[1]))
+   end
+   for _, toyInfo in pairs(ns.toys) do
+      table.insert(self.items, Item:CreateFromItemID(toyInfo[1]))
+   end
+end
+
+---@param callback function
+function ns:LoadItemData(callback)
+   self.loaded = true
+   if not self.items then
+      self:CreateItems()
+   end
+   local loader = ContinuableContainer:Create()
+   DevTool:AddData(self.items, 'items')
+   loader:AddContinuables(self.items)
+   loader:ContinueOnLoad(function()
+      self.data = {
+         weapons = {},
+         appearances = {},
+         toys = {},
+         mounts = {},
+      }
+      for equip, ids in pairs(ns.weapons) do
+         self.data.weapons[equip] = {}
+         for id, loc in pairs(ids) do
+            self.data.weapons[equip][id] = {
+               link = select(2, C_Item.GetItemInfo(id)),
+               has = C_TransmogCollection.PlayerHasTransmog(id),
+               location = loc
+            }
+         end
+      end
+      for vendor, ids in pairs(ns.appearances) do
+         self.data.appearances[vendor] = {}
+         for _, appearanceInfo in pairs(ids) do
+            if vendor == "bones" then
+               self.data.appearances[vendor][appearanceInfo[1]] = {
+                  link = select(2, C_Item.GetItemInfo(appearanceInfo[1])),
+                  has = C_TransmogCollection.PlayerHasTransmog(appearanceInfo[1]),
+                  cost = appearanceInfo[2],
+                  remaining = appearanceInfo[3],
+               }
+            else
+               local has, remaining = hasEnsemble(appearanceInfo[1])
+               self.data.appearances[vendor][appearanceInfo[1]] = {
+                  link = select(2, C_Item.GetItemInfo(appearanceInfo[1])),
+                  cost = appearanceInfo[2],
+                  has = has or false,
+                  remaining = remaining or 0,
+               }
+            end
+         end
+      end
+      for _, toyInfo in pairs(ns.toys) do
+         self.data.toys[toyInfo[1]] = {
+            link = select(2, C_Item.GetItemInfo(toyInfo[1])),
+            has = PlayerHasToy(toyInfo[1]),
+            cost = toyInfo[2],
+         }
+      end
+      for _, mountInfo in pairs(ns.mounts) do
+         local mountID = C_MountJournal.GetMountFromItem(mountInfo[1]) --[[@as number]]
+         if mountID then
+            self.data.mounts[mountInfo[1]] = {
+               link = select(2, C_Item.GetItemInfo(mountInfo[1])),
+               has = select(11, C_MountJournal.GetMountInfoByID(mountID)),
+               cost = mountInfo[2],
+            }
+         end
+      end
+      DevTool:AddData(self.data, 'collection')
+      if callback then
+         callback()
+      end
+   end)
+end
--- mixins.lua
+++ mixins.lua
@@ -1,6 +1,6 @@
 
 ---@class ns
-local addon = select(2, ...)
+local ns = select(2, ...)
 
 ---@class RemixWeaponsFrame : Frame
 ---@field ScrollBox Frame
@@ -8,13 +8,13 @@ local addon = select(2, ...)
 ---@field CloseButton Button
 RemixWeaponsFrameMixin = {}
 
-local DataProvider = CreateTreeDataProvider()
-local ScrollView = CreateScrollBoxListTreeListView()
 function RemixWeaponsFrameMixin:OnLoad()
+   self.dataProvider = CreateTreeDataProvider()
+   self.scrollView = CreateScrollBoxListTreeListView()
+   self.dataProvider:CollapseAll()
+   self.scrollView:SetDataProvider(self.dataProvider)
 
-   ScrollView:SetDataProvider(DataProvider)
-
-   ScrollUtil.InitScrollBoxListWithScrollBar(self.ScrollBox, self.ScrollBar, ScrollView)
+   ScrollUtil.InitScrollBoxListWithScrollBar(self.ScrollBox, self.ScrollBar, self.scrollView)
    local withBar = {
       CreateAnchor("TOPLEFT", self, "TOPLEFT", 10, -22),
       CreateAnchor("BOTTOMRIGHT", self, "BOTTOMRIGHT", -22, 1)
@@ -24,60 +24,87 @@ function RemixWeaponsFrameMixin:OnLoad()
       CreateAnchor("BOTTOMRIGHT", self, "BOTTOMRIGHT", -2, 1)
    }
    ScrollUtil.AddManagedScrollBarVisibilityBehavior(self.ScrollBox, self.ScrollBar, withBar, withoutBar)
-end
-
-function RemixWeaponsFrameMixin:OnEvent()
-end
-
-local function Initializer(frame, node)
-   node:SetCollapsed(true, true)
-   frame:SetText(node:GetData().ButtonText)
-   frame:SetScript("OnClick", function()
-      local data = node:GetData()
-      if data.topLevel and not data.loaded then
-         local continuable = ContinuableContainer:Create()
-         for itemID in pairs(addon.weapons[node:GetData().key]) do
-            continuable:AddContinuable(Item:CreateFromItemID(itemID))
-         end
-         continuable:ContinueOnLoad(function()
-            for itemID, loc in pairs(addon.weapons[node:GetData().key]) do
-               local tooltip = C_TooltipInfo.GetItemByID(itemID)
-               print(tooltip.hyperlink)
-               local appearanceKnown = true
-               for i = #tooltip.lines, 1, -1 do
-                  local line = tooltip.lines[i]
-                  if (line.leftText and line.leftText:find(TRANSMOGRIFY_TOOLTIP_APPEARANCE_UNKNOWN)) or (line.rightText and line.rightText:find(TRANSMOGRIFY_TOOLTIP_APPEARANCE_UNKNOWN)) then
-                     appearanceKnown = false
-                     break
-                  end
-               end
-               node:Insert({
-                  ButtonText = tooltip.hyperlink .. " " .. loc .. " " .. (appearanceKnown and ":)" or ":("),
-                  key = itemID
-               })
-            end
-            data.loaded = true
-         end)
-      end
-      node:ToggleCollapsed()
-      print("Clicked", node:GetData().key)
-   end)
-end
-
-local function CustomFactory(factory, node)
-   factory("UIPanelButtonTemplate", Initializer)
-end
-
-ScrollView:SetElementFactory(CustomFactory)
-
-
-for equip in pairs(addon.weapons) do
-   local topLevelData = {
-      ButtonText = addon.enum.equipName[equip] or ("Invalid Equip type %q"):format(equip or "UNKNOWN"),
-      topLevel = true,
-      key = equip
-   }
-   DataProvider:Insert(topLevelData)
+   
+   
+   local function Initializer(frame, node)
+      frame:SetText(node:GetData().ButtonText)
+      frame:SetScript("OnClick", function()
+         node:ToggleCollapsed()
+      end)
+   end
+   
+   local function CustomFactory(factory, node)
+      factory("UIPanelButtonTemplate", Initializer)
+   end
+   
+   self.scrollView:SetElementFactory(CustomFactory)
    
 end
 
+function RemixWeaponsFrameMixin:Populate()
+   self.dataProvider:Flush()
+   local weaponNode = self.dataProvider:Insert({
+      ButtonText = "Weapons",
+      topLevel = true,
+   })
+
+   for equip, checkList in pairs(ns.data.weapons) do
+      local equipNode = weaponNode:Insert({
+         ButtonText = ns.enum.equipName[equip] or ("Invalid Equip type %q"):format(equip or "UNKNOWN"),
+         topLevel = true,
+         key = equip
+      })
+      for id, checkListItem in pairs(checkList) do
+         equipNode:Insert({
+            ButtonText = checkListItem.link .. " " .. (checkListItem.has and ":)" or ":(") .. " " .. checkListItem.location,
+            key = id
+         })
+      end
+   end
+   local appearanceNode = self.dataProvider:Insert({
+      ButtonText = "Appearances",
+      topLevel = true,
+   })
+   for vendor, checkList in pairs(ns.data.appearances) do
+      local equipNode = appearanceNode:Insert({
+         ButtonText = vendor,
+         key = vendor
+      })
+      for id, checkListItem in pairs(checkList) do
+         equipNode:Insert({
+            ButtonText = checkListItem.link .. " " .. (checkListItem.has and ":)" or ":(") .. " " .. checkListItem.remaining .. " " .. checkListItem.cost,
+            key = id
+         })
+      end
+   end
+   local toyNode = self.dataProvider:Insert({
+      ButtonText = "Toys",
+      topLevel = true,
+   })
+   for id, checkListItem in pairs(ns.data.toys) do
+      toyNode:Insert({
+         ButtonText = checkListItem.link .. " " .. (checkListItem.has and ":)" or ":(") .. " " .. checkListItem.cost,
+         key = id
+      })
+   end
+   local mountNode = self.dataProvider:Insert({
+      ButtonText = "Mounts",
+      topLevel = true,
+   })
+   for id, checkListItem in pairs(ns.data.mounts) do
+      mountNode:Insert({
+         ButtonText = checkListItem.link .. " " .. (checkListItem.has and ":)" or ":(") .. " " .. checkListItem.cost,
+         key = id
+      })
+   end
+   self.dataProvider:CollapseAll()
+end
+
+
+function RemixWeaponsFrameMixin:OnMouseDown()
+   self:StartMoving()
+end
+
+function RemixWeaponsFrameMixin:OnMouseUp()
+   self:StopMovingOrSizing()
+end
